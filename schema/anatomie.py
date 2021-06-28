1c6db4de122bb83318902d05ee720b80bc32ae75
Ken <ken.murabe@gmail.com>
2021-06-28 21:02:13 +0900

    Update anatomie.py

--- schema/anatomie.py
+++ schema/anatomie.py
@@ -1,134 +1,233 @@
 {
 	'guid': {
 		'required': True,
-		'type': 'string'
+		'type': 'string',
+		'meta': {
+			'fieldName': "guid"
+		}
 	},
 	'noteid': {
 		'required': True,
-		'type': 'string'
+		'type': 'string',
+		'regex': '[0-9]{13}$',
+		'meta': {
+			'fieldName': "noteid"
+		}
 	},
 	'Title': {
 		'required': True,
-		'type': 'string'
+		'type': 'string',
+		'meta': {
+			'fieldName': "Title"
+		}
 	},
 	'OccludedImage': {
 		'required': True,
-		'type': 'string'
+		'type': 'string',
+		'meta': {
+			'fieldName': "OccludedImage"
+		}
 	},
 	'Clinical': {
 		'required': True,
-		'type': 'string'
+		'type': 'string',
+		'meta': {
+			'fieldName': "Clinical"
+		}
 	},
 	'Comment': {
 		'required': True,
-		'type': 'string'
+		'type': 'string',
+		'meta': {
+			'fieldName': "Comment"
+		}
 	},
 	'Origin': {
 		'required': True,
-		'type': 'string'
+		'type': 'string',
+		'meta': {
+			'fieldName': "Origin"
+		}
 	},
 	'Insertion': {
 		'required': True,
-		'type': 'string'
+		'type': 'string',
+		'meta': {
+			'fieldName': "Insertion"
+		}
 	},
 	'Attachments': {
 		'required': True,
-		'type': 'string'
+		'type': 'string',
+		'meta': {
+			'fieldName': "Attachments"
+		}
 	},
 	'Innervation': {
 		'required': True,
-		'type': 'string'
+		'type': 'string',
+		'meta': {
+			'fieldName': "Innervation"
+		}
 	},
 	'Actions': {
 		'required': True,
-		'type': 'string'
+		'type': 'string',
+		'meta': {
+			'fieldName': "Actions"
+		}
 	},
 	'Plate': {
 		'required': True,
-		'type': 'string'
+		'type': 'string',
+		'meta': {
+			'fieldName': "Plate"
+		}
 	},
 	'1a': {
 		'required': True,
-		'type': 'string'
+		'type': 'string',
+		'meta': {
+			'fieldName': "1a"
+		}
 	},
 	'2a': {
 		'required': True,
-		'type': 'string'
+		'type': 'string',
+		'meta': {
+			'fieldName': "2a"
+		}
 	},
 	'3a': {
 		'required': True,
-		'type': 'string'
+		'type': 'string',
+		'meta': {
+			'fieldName': "3a"
+		}
 	},
 	'4a': {
 		'required': True,
-		'type': 'string'
+		'type': 'string',
+		'meta': {
+			'fieldName': "4a"
+		}
 	},
 	'5a': {
 		'required': True,
-		'type': 'string'
+		'type': 'string',
+		'meta': {
+			'fieldName': "5a"
+		}
 	},
 	'6a': {
 		'required': True,
-		'type': 'string'
+		'type': 'string',
+		'meta': {
+			'fieldName': "6a"
+		}
 	},
 	'7a': {
 		'required': True,
-		'type': 'string'
-	},
+		'type': 'string',
+		'meta': {
+			'fieldName': "7a"
+		}
+	},	
 	'8a': {
 		'required': True,
-		'type': 'string'
+		'type': 'string',
+		'meta': {
+			'fieldName': "8a"
+		}
 	},
 	'9a': {
 		'required': True,
-		'type': 'string'
+		'type': 'string',
+		'meta': {
+			'fieldName': "9a"
+		}
 	},
 	'10a': {
 		'required': True,
-		'type': 'string'
+		'type': 'string',
+		'meta': {
+			'fieldName': "10a"
+		}
 	},
 	'11a': {
 		'required': True,
-		'type': 'string'
+		'type': 'string',
+		'meta': {
+			'fieldName': "11a"
+		}
 	},
 	'12a': {
 		'required': True,
-		'type': 'string'
+		'type': 'string',
+		'meta': {
+			'fieldName': "12a"
+		}
 	},
 	'13a': {
 		'required': True,
-		'type': 'string'
+		'type': 'string',
+		'meta': {
+			'fieldName': "13a"
+		}
 	},
 	'14a': {
 		'required': True,
-		'type': 'string'
+		'type': 'string',
+		'meta': {
+			'fieldName': "14a"
+		}
 	},
 	'15a': {
 		'required': True,
-		'type': 'string'
+		'type': 'string',
+		'meta': {
+			'fieldName': "15a"
+		}
 	},
 	'16a': {
 		'required': True,
-		'type': 'string'
+		'type': 'string',
+		'meta': {
+			'fieldName': "16a"
+		}
 	},
 	'17a': {
 		'required': True,
-		'type': 'string'
+		'type': 'string',
+		'meta': {
+			'fieldName': "17a"
+		}
 	},
 	'18a': {
 		'required': True,
-		'type': 'string'
+		'type': 'string',
+		'meta': {
+			'fieldName': "18a"
+		}
 	},
 	'19a': {
 		'required': True,
-		'type': 'string'
+		'type': 'string',
+		'meta': {
+			'fieldName': "19a"
+		}
 	},
 	'20a': {
 		'required': True,
-		'type': 'string'
+		'type': 'string',
+		'meta': {
+			'fieldName': "20a"
+		}
 	},
 	'tags': {
 		'required': True,
-		'type': 'list'
-	}
+		'type': 'list',
+		'meta': {
+			'fieldName': "tags"
+		}
 }
